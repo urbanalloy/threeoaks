7c1d80e92512f42b8dfd4f46527256534b5be5b7
julien.templier <julien.templier@ab80709b-eb45-0410-bb3a-633ce738720d>
2008-07-03 15:10:32 +0000

    Added VB Sample docklet

---
+++ trunk/ObjectDock/Docklets/DotNet/Samples/DummyVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DummyVB")> 
+<Assembly: AssemblyDescription("A Dummy .Net Docklet, used to test the .Net Docklet SDK.")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("DummyVB")> 
+<Assembly: AssemblyCopyright("Copyright ©  2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b47b2327-552f-460d-92f8-02e164250c86")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
