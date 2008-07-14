7b7f4769972e5bb638c99ce7b8b34ddc180d2fa6
julien.templier <julien.templier@ab80709b-eb45-0410-bb3a-633ce738720d>
2008-07-14 21:45:14 +0000

    Updated headers Added Std.c to project and removed duplicate inline function

--- ScreenSavers/Flurry/Sources/Core/Std.c
+++ ScreenSavers/Flurry/Sources/Core/Std.c
@@ -1,15 +1,45 @@
-// Precompiled "Std" Include Header.
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Flurry
+//
+// Copyright (c) 2000, Aaron Hilton
+// All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+// * $LastChangedRevision$
+// * $LastChangedDate$
+// * $LastChangedBy$
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// o Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// o Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// o Neither the name of the author nor the names of its contributors may
+//   be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "Std.h"
 
-//UInt8  sys_glBPP=32;
-//int SSMODE = FALSE;
-//int currentVideoMode = 0;
-//int cohesiveness = 7;
-//int fieldStrength;
-//int colorCoherence = 7;
-//int fieldIncoherence = 0;
-//int ifieldSpeed = 120;
 
 __private_extern__ __inline float FastDistance2D(float x, float y)
 {
@@ -20,10 +50,10 @@ __private_extern__ __inline float FastDistance2D(float x, float y)
 	y = (y < 0.0f) ? -y : y;
 	
 	// compute the minimum of x,y
-	mn = x<y?x:y;
+	mn = (x < y) ? x : y;
 	
 	// return the distance
 	//return(x+y-(mn*0.5f)-(mn*0.25f)+(mn*0.0625f));
-	return x+y-mn*.6875;
+	return x + y - mn * 0.6875f;
 	
 }
