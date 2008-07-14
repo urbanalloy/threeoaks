9bed97e61dfd1bf01dd3ae5733797516db801b74
julien.templier <julien.templier@ab80709b-eb45-0410-bb3a-633ce738720d>
2008-07-14 21:45:14 +0000

    Updated headers Added Std.c to project and removed duplicate inline function

--- trunk/ScreenSavers/Flurry/Sources/Core/Std.h
+++ trunk/ScreenSavers/Flurry/Sources/Core/Std.h
@@ -1,38 +1,51 @@
-//-------------------------------------------------------------------
-//  File:   Std.h
-//  Created:  02/12/00 9:01:PM
-//  Author:   Aaron Hilton
-//  Comments: Standard header file to include all source files.
-//            (Precompiled header)
-//-------------------------------------------------------------------
-#ifndef __STD_h_
-#define __STD_h_
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Flurry
+//
+// Copyright (c) 2000, Aaron Hilton
+// All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+// * $LastChangedRevision$
+// * $LastChangedDate$
+// * $LastChangedBy$
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// o Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// o Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// o Neither the name of the author nor the names of its contributors may
+//   be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
 
 #include <math.h>
 #include <stdlib.h>
 #include "PTypes.h"
 #include "Gl_saver.h"
 
-
-INLINE float FastDistance2D(float x, float y)
-{
-	// this function computes the distance from 0,0 to x,y with ~3.5% error
-	float mn;
-	// first compute the absolute value of x,y
-	x = (x < 0.0f) ? -x : x;
-	y = (y < 0.0f) ? -y : y;
-	
-	// compute the minimum of x,y
-	mn = (x < y) ? x : y;
-	
-	// return the distance
-	return x + y - mn * 0.6875f;
-	
-}
-
+INLINE float FastDistance2D(float x, float y);
 
 #define RandFlt(min, max) (min + (max - min) * rand() / (float) RAND_MAX)
 #define RandBell(scale) (scale * (1.0f - (rand() + rand() + rand()) / ((float) RAND_MAX * 1.5f)))
-
-
-#endif // __STD_h_
--- trunk/ScreenSavers/Flurry/Sources/Core/Texture.h
+++ trunk/ScreenSavers/Flurry/Sources/Core/Texture.h
@@ -1,22 +1,48 @@
-/*
- *  Texture.h
- *  AppleFlurry
- *
- *  Created by Mike Trent on Wed May 22 2002.
- *  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
- *
- */
-
-#ifndef __TEXTURE_H_
-#define __TEXTURE_H_
-
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Flurry
+//
 // Texture builds the textures used by the Flurry streamer. The texture is loaded
 // directly into OpenGL.
+//
+// Copyright (c) 2002, Mike Trent
+// All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+// * $LastChangedRevision$
+// * $LastChangedDate$
+// * $LastChangedBy$
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// o Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// o Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+// o Neither the name of the author nor the names of its contributors may
+//   be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
 
+#pragma once
 
 // MakeTexture is a big do-all function that creates the textures and leaves them
 // ready for use.
 __private_extern__ void MakeTexture(void);
-
-
-#endif // __TEXTURE_H_
