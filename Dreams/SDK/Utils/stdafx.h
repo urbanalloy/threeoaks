00ddb456e855861536011d669905c3299d83ce7f
julien.templier <julien.templier@ab80709b-eb45-0410-bb3a-633ce738720d>
2008-09-12 15:30:39 +0000

    Removed duplicated pragma

--- Dreams/SDK/Utils/stdafx.h
+++ Dreams/SDK/Utils/stdafx.h
@@ -5,7 +5,6 @@
 
 #pragma once
 
-#pragma once
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
 #ifndef WINVER                          // Allow use of features specific to Windows Vista or later.
